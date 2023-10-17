e368c022bfe78a3ef34039000dcbe5f07ededd80
Louis Moses <louismoses.rait@gmail.com>
2023-10-17 21:22:59 +0800

    update: set Page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import CreateBook from "./pages/CreateBooks";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+
 function App() {
   return (
     <Routes>
-      <Route path="" element{} />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
 }
