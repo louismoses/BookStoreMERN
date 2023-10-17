7d6869bf7620e235fa44de7e11f7b8d627433366
Louis Moses <louismoses.rait@gmail.com>
2023-10-17 20:31:11 +0800

    update: refactor routes and use express middleware

--- backend/index.js
+++ backend/index.js
@@ -3,73 +3,18 @@ import mongoose from "mongoose";
 
 import { PORT, mongoDBURL } from "./config.js";
 import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
 // middleware for parsing request body
 app.use(express.json());
 
+app.use("/books", booksRoute);
+
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to MERN");
-});
-
-// route to Save a new Book
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({
-        message: "Send all required fields",
-      });
-    }
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-
-    return res.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-// route to get all books
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find();
-    return res.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-// route to get book by ID
-app.get("/books/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const book = await Book.findById(id);
-    return res.status(200).json(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-// route to update a book
-app.put("/books/:id", (req, res) => {
-  try {
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
 });
 
 mongoose
