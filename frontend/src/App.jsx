1823cd7f53b2f989892ac3ce65a50884243f6242
Louis Moses <louismoses.rait@gmail.com>
2023-10-17 21:04:52 +0800

    update: frontend commit, setup react routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="" element{} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Routes>
+  );
+}
+
+export default App;
