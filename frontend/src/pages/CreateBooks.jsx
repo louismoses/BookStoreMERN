016cf2aa51ee812efb0a1dcaadc368c8fbfea2d8
Louis Moses <louismoses.rait@gmail.com>
2023-10-18 15:44:55 +0800

    update: add input field for book detail upload

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -10,10 +10,44 @@ const CreateBooks = () => {
   const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const handelSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:5555/books", data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+        alert("An error occured. Check console for error message");
+        setLoading(false);
+      });
+  };
 
   return (
-    <div>
-      <Spinner />
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label htmlFor="" className="text-xl mr-4 text-gray-500">
+            Title
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+      </div>
     </div>
   );
 };
